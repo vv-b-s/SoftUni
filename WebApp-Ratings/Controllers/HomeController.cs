514cd2bdc06eed8fe4a8b0aca92df92c4555d285
Valentin Vasilev <valentin.v1997@gmail.com>
2017-04-09 18:17:19 +0300

    Rating WebApp added

---
+++ WebApp-Ratings/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_Ratings.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult DrawRating(int rating)
+        {
+            var fullStars = rating * 0.1;
+            var emptyStars = (100 - rating) * 0.1;
+            var halfStars = 10 - fullStars - emptyStars;
+
+            var stars = "";
+            for (int i = 0; i < fullStars; i++)
+                stars += "<img src='/images/full-star.png' />";
+            for (int i = 0; i < halfStars; i++)
+                stars += "<img src='/images/half-star.png' />";
+            for (int i = 0; i < emptyStars; i++)
+                stars += "<img src='/images/empty-star.png' />";
+
+            ViewBag.Stars = stars;
+            ViewBag.Rating = rating;
+
+            return View("Index", "",stars);
+        }
+    }
+}
