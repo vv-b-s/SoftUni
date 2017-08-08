afc62c22711e017a1320b5f2cb436ad7df865e8d
vv-b-s <valentin.v1997@gmail.com>
2017-08-08 18:49:55 +0300

    Added: C# MVC Lab

---
+++ Software Technologies/C# MVC/LAB/TODOList/TODOList/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TODOList.Models
+{
+    public class Task
+    {
+        public int Id { set; get; }
+
+        [Required]
+        public string Title { set; get; }
+    }
+}
