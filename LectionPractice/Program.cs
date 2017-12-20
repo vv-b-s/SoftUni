f0f6799476554363c5ab91b1ac5796c187b10998
vv-b-s <valentin.v1997@gmail.com>
2017-12-20 02:41:07 +0200

    Added sets and dictionaries lab

--- LectionPractice/Program.cs
+++ LectionPractice/Program.cs
@@ -6,9 +6,21 @@ using System.Linq;
 using static System.Console;
 using System.Globalization;
 
-class Program
+class CountSameValuesInArray
 {
     static void Main()
     {
+        var numberOfStudents = int.Parse(ReadLine());
+        var studentsData = new SortedDictionary<string, double[]>();
+
+        while(numberOfStudents-->0)
+        {
+            string studentName = ReadLine();
+            double[] studentGrades = ReadLine().Split(' ').Where(i=>i!="").Select(double.Parse).ToArray();
+            studentsData[studentName] = studentGrades;
+        }
+
+        foreach (var student in studentsData)
+            WriteLine($"{student.Key} is graduated with {student.Value.Average()}");
     }
 }
